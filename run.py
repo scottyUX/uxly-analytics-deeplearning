4ad6e512f426553c7bff9224aa59ad200e8a0757
muratcancicek <muratcancicek0@gmail.com>
2024-06-07 10:06:27 +0300

    Restructured the project folders

--- run.py
+++
@@ -1,8 +0,0 @@
-from bot_detection.bot_detection import BotDetection
-from graph_query.query_degen_airdrop_network import main
-
-
-if __name__ == "__main__":
-    # bot_detection = BotDetection()
-    # bot_detection.run()
-    main()
---
+++ run_bot_detection.py
@@ -0,0 +1,6 @@
+from bot_detection.bot_detection import BotDetection
+
+
+if __name__ == "__main__":
+    bot_detection = BotDetection()
+    bot_detection.run()
