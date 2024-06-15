1c62495f2024680998ff103984bb3d757406b28d
muratcancicek <muratcancicek0@gmail.com>
2024-06-15 20:16:29 +0300

    Utilized BaseModel

--- airdrop_analysis/data_handler/models/base_models/wallet.py
+++ airdrop_analysis/data_handler/models/base_models/wallet.py
@@ -1,9 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
 from data_handler.models.base_models.transaction_history \
     import TransactionHistory
 from utils.custom_keys import CustomKeys as ck
 
 
-class WalletStats(object):
+class WalletStats(BaseModel):
+    address: str
+    chain: str
+    nft_count: int
+    collection_count: int
+    transaction_count: int
+    nft_transfer_count: int
+    token_transfer_count: int
 
     @staticmethod
     def from_dict(response: dict):
@@ -17,7 +26,6 @@ class WalletStats(object):
             token_transfer_count = int(response[ck.TOKEN_TRANSFER_COUNT]),
         )
 
-
     @staticmethod
     def from_moralis_dict(address: str, chain: str, response: dict):
         return WalletStats(
@@ -30,51 +38,17 @@ class WalletStats(object):
             token_transfer_count = int(response[ck.TOKEN_TRANSFERS][ck.TOTAL]),
         )
 
-    def __init__(
-            self, 
-            address: str,
-            chain: str,
-            nft_count: int,
-            collection_count: int,
-            transaction_count: int,
-            nft_transfer_count: int,
-            token_transfer_count: int,
-            ):
-        self.address = address
-        self.chain = chain
-        self.nft_count = nft_count
-        self.collection_count = collection_count
-        self.transaction_count = transaction_count
-        self.nft_transfer_count = nft_transfer_count
-        self.token_transfer_count = token_transfer_count
+    def to_dict(self):
+        return self.model_dump(exclude_unset=True)
+
+
+class Wallet(BaseModel):
+    address: str
+    transaction_history: TransactionHistory
+    stats: Optional[WalletStats] = None
 
     def to_dict(self):
-        return {
-            ck.ADDRESS: self.address,
-            ck.CHAIN: self.chain,
-            ck.NFT_COUNT: self.nft_count,
-            ck.COLLECTION_COUNT: self.collection_count,
-            ck.TRANSACTION_COUNT: self.transaction_count,
-            ck.NFT_TRANSFER_COUNT: self.nft_transfer_count,
-            ck.TOKEN_TRANSFER_COUNT: self.token_transfer_count,
-        }
+        return self.model_dump(exclude_unset=True)
 
-
-class Wallet(object):
-
-    def __init__(
-            self,
-            address: str,
-            transaction_history: TransactionHistory,
-            stats: WalletStats = None,
-    ):
-        self.address = address
-        self.stats = stats
-        self.transaction_history = transaction_history
-
-    def to_dict(self):
-        d = { ck.ADDRESS: self.address }
-        d[ck.TRANSACTIONS] = self.transaction_history.to_dict()
-        if self.stats is not None:
-            d[ck.STATS] = self.stats.to_dict()
-        return d
+    class Config:
+        arbitrary_types_allowed = True
